160f6363bbd87454b7ef68a1d7b86658a68c7a48
GAMING\Admin <jjrosenberg122@gmail.com>
2020-09-01 12:18:42 +0200

    Main Menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public string startGame;
+    public string options;
+    public string credits;
+    public string information;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene(startGame);
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene(options);
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene(credits);
+    }
+
+    public void Information()
+    {
+        SceneManager.LoadScene(information);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
