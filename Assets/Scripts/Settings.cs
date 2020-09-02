b774c0c11ea206be62b2875b1dd088e4e8419e4c
jo2n64 <valio31032@gmail.com>
2020-09-02 14:29:40 +0200

    added settings for real this time

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -21,13 +21,24 @@ public class Settings : MonoBehaviour
         
         List<string> options = new List<string>();
 
+        int currResolutionIndex = 0;
+        
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currResolutionIndex = i;
+            }
+            
         }
         
-        resolutionDropdown.AddOptions(options);    
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     public void ToggleFullScreen(bool isFullscreen)
@@ -39,4 +50,11 @@ public class Settings : MonoBehaviour
     {
         audioMixer.SetFloat("Volume", value);
     }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    
 }
