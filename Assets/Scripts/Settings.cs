b3ce4cf4bb2e3df3b9066cdb37cf6306c5308315
jo2n64 <valio31032@gmail.com>
2020-09-02 14:24:10 +0200

    added options menu

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,12 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
+    public AudioMixer audioMixer;
     // Start is called before the first frame update
+    public Dropdown resolutionDropdown;
+    
+    private Resolution[] resolutions;
+    
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        
+        resolutionDropdown.ClearOptions();
+        
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+        
+        resolutionDropdown.AddOptions(options);    
+    }
+
     public void ToggleFullScreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetVolume(float value)
+    {
+        audioMixer.SetFloat("Volume", value);
     }
 }
