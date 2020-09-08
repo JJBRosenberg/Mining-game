39cf7b4d591becd7f489590b196bbd92dc97fc69
GAMING\Admin <jjrosenberg122@gmail.com>
2020-09-08 15:20:49 +0200

    Instruction sheet popup for the start of levels

---
+++ Assets/Scripts/openingScreen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class openingScreen : MonoBehaviour
+    
+{
+    [SerializeField] public Canvas instructionCanvas;
+    [SerializeField] public Canvas HUD;
+    [SerializeField] private Player player;
+    [SerializeField] private CameraController cameraController;
+    void Awake()
+    {
+        instructionCanvas.gameObject.SetActive(true);
+        HUD.gameObject.SetActive(false);
+        //Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        //player.enabled = false;
+        //cameraController.enabled = false;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Debug.Log("hi");
+            instructionCanvas.gameObject.SetActive(false);
+            HUD.gameObject.SetActive(true);
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            player.enabled = true;
+            cameraController.enabled = true;
+        }
+    }
+    
+}
