3c8d9d55ed53d03a42874695744fe1f012e85980
ReportMex9 <jjrosenberg122@gmail.com>
2020-09-10 13:21:05 +0200

    no message

--- Assets/Scripts/openingScreen.cs
+++ Assets/Scripts/openingScreen.cs
@@ -13,18 +13,17 @@ public class openingScreen : MonoBehaviour
         HUD.gameObject.SetActive(false);
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         cameraController = player.GetComponentInChildren<CameraController>();
-        Time.timeScale = 0;
-        Cursor.lockState = CursorLockMode.None;
-        player.enabled = false;
-        cameraController.enabled = false;
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        player.enabled = true;
+        cameraController.enabled = true;
 
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.Mouse0))
-        {
-            Debug.Log("hi");
+        {   
             instructionCanvas.gameObject.SetActive(false);
             HUD.gameObject.SetActive(true);
             Time.timeScale = 1;
